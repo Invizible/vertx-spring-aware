2555bad13d7ba33e9ca497da346797204344e891
Korobko Alex <korobkoalexander5901@gmail.com>
2017-01-15 19:56:45 +0200

    added logging

--- src/main/java/com/github/invizible/VerticleDeployBeanPostProcessor.java
+++ src/main/java/com/github/invizible/VerticleDeployBeanPostProcessor.java
@@ -1,6 +1,7 @@
 package com.github.invizible;
 
 import io.vertx.core.Vertx;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 /**
  * @author Alex
  */
+@CommonsLog
 public class VerticleDeployBeanPostProcessor implements BeanPostProcessor {
 
   @Autowired
@@ -17,9 +19,16 @@ public class VerticleDeployBeanPostProcessor implements BeanPostProcessor {
     Class<?> clazz = bean.getClass();
     if (clazz.isAnnotationPresent(Verticle.class)) {
       if (bean instanceof io.vertx.core.Verticle) {
-        vertx.deployVerticle((io.vertx.core.Verticle) bean);
+        log.info(String.format("Deploying Verticle: %s", clazz.getName()));
+        vertx.deployVerticle((io.vertx.core.Verticle) bean, result -> {
+          if (result.succeeded()) {
+            log.info(String.format("Verticle %s has been deployed successfully", clazz.getName()));
+          } else {
+            log.error(String.format("Verticle %s has not been deployed!", clazz.getName()), result.cause());
+          }
+        });
       } else {
-        vertx.deployVerticle(clazz.getName());
+        log.error(String.format("Incorrect Verticle class %s. Class marked with @Verticle annotation should implement io.vertx.core.Verticle", clazz.getName()));
       }
     }
 
